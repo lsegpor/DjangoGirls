563a808ffc069ac1b6a9e165460858f46909ba9a
lsegura <luciasegpor03@gmail.com>
2025-04-22 15:40:46 +0200

    post_details.html

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,11 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
-    path("", views.post_list, name="post_list"),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = (
+    [
+        path("", views.post_list, name="post_list"),
+        path("post/<int:pk>/", views.post_detail, name="post_detail"),
+    ]
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
